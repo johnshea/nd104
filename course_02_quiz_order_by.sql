0cbc16df4b4d331c18b38fc0de813470a26d6ec5
John Shea <sheajohnh@gmail.com>
2026-02-19 12:48:22 -0500

    Write query for question 3

--- course_02_quiz_order_by.sql
+++ course_02_quiz_order_by.sql
@@ -9,3 +9,9 @@ SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY total_amt_usd DESC
 LIMIT 5;
+
+-- 3. Write a query to return the lowest 20 orders in terms of the smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd ASC
+LIMIT 20;
