37ebf71fd724473149bf96eab32a56bb16662f1c
John Shea <sheajohnh@gmail.com>
2026-02-19 12:45:03 -0500

    Create sql queries for order by quiz

---
+++ course_02_quiz_order_by.sql
@@ -0,0 +1,11 @@
+-- 1. Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at ASC
+LIMIT 10;
+
+-- 2. Write a query to return the top 5 orders in terms of the largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC
+LIMIT 5;
