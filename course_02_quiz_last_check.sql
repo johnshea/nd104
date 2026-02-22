dd7ecdb659cf2dd8de831fb03264ade6eee15920
John Shea <sheajohnh@gmail.com>
2026-02-21 21:04:49 -0500

    Write second query for last check quiz

--- course_02_quiz_last_check.sql
+++ course_02_quiz_last_check.sql
@@ -7,3 +7,14 @@ JOIN accounts AS a
 ON a.sales_rep_id = sr.id
 WHERE r.name = 'Midwest'
 ORDER BY a.name ASC;
+
+-- 2. Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first name starting with S and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to the account name.
+SELECT sr.name AS sales_rep_name, r.name AS region_name, a.name AS account_name
+FROM sales_reps AS sr 
+JOIN region AS r
+ON sr.region_id = r.id
+JOIN accounts AS a
+ON a.sales_rep_id = sr.id
+WHERE r.name = 'Midwest'
+  AND sr.name LIKE 'S%'
+ORDER BY a.name ASC;
