d3e745a2fd1585673b121845df13258a2d6e1101
John Shea <sheajohnh@gmail.com>
2026-02-22 09:48:14 -0500

    Write fourth query for last check quiz

--- course_02_quiz_last_check.sql
+++ course_02_quiz_last_check.sql
@@ -29,3 +29,14 @@ ON a.sales_rep_id = sr.id
 WHERE r.name = 'Midwest'
   AND sr.name LIKE '% K%'
 ORDER BY a.name ASC;
+
+-- 4. Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100. Your final table should have 3 columns: region name, account name, and unit price. In order to avoid a division by zero error, adding .01 to the denominator here is helpful total_amt_usd/(total+0.01).
+SELECT r.name AS region_name, a.name AS account_name, o.total_amt_usd/(o.total + 0.01) AS unit_price
+FROM orders AS o
+JOIN accounts AS a
+ON o.account_id = a.id
+JOIN sales_reps AS sr
+ON a.sales_rep_id = sr.id
+JOIN region AS r
+ON sr.region_id = r.id
+WHERE o.standard_qty > 100;
