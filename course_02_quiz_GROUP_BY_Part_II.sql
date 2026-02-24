b37920299aef330f61b2a59b254b2c28ca3557ee
John Shea <sheajohnh@gmail.com>
2026-02-24 14:13:55 -0500

    Write 2nd query for GROUP BY Part II quiz

--- course_02_quiz_GROUP_BY_Part_II.sql
+++ course_02_quiz_GROUP_BY_Part_II.sql
@@ -4,3 +4,10 @@ FROM accounts AS a
 JOIN orders AS o
 ON o.account_id = a.id
 GROUP BY a.id, a.name;
+
+-- 2. For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type.
+SELECT a.name, AVG(o.standard_amt_usd), AVG(o.gloss_amt_usd), AVG(o.poster_amt_usd)
+FROM accounts AS a
+JOIN orders AS o
+ON o.account_id = a.id
+GROUP BY a.id, a.name;
