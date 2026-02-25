76946d16ea441097ea82c7b288dec23c64cef394
John Shea <sheajohnh@gmail.com>
2026-02-25 18:26:32 -0500

    Write 3rd/4th/5th queries for date functions quiz

--- course_02_quiz_date_functions.sql
+++ course_02_quiz_date_functions.sql
@@ -9,3 +9,24 @@ SELECT DATE_PART('month', o.occurred_at), SUM(o.total_amt_usd)
 FROM orders AS o
 GROUP BY DATE_PART('month', o.occurred_at)
 ORDER BY SUM(o.total_amt_usd) DESC;
+
+-- 3. Which year did Parch & Posey have the greatest sales in terms of the total number of orders? Are all years evenly represented by the dataset?
+SELECT DATE_PART('year', o.occurred_at), SUM(o.total_amt_usd)
+FROM orders AS o
+GROUP BY DATE_PART('year', o.occurred_at)
+ORDER BY SUM(o.total_amt_usd) DESC;
+
+-- 4. Which month did Parch & Posey have the greatest sales in terms of the total number of orders? Are all months evenly represented by the dataset?
+SELECT DATE_PART('month', o.occurred_at), COUNT(*)
+FROM orders AS o
+GROUP BY DATE_PART('month', o.occurred_at)
+ORDER BY COUNT(*) DESC;
+
+-- 5. In which month of which year did Walmart spend the most on gloss paper in terms of dollars?
+SELECT DATE_TRUNC('month', o.occurred_at), SUM(o.gloss_amt_usd)
+FROM orders AS o
+JOIN accounts AS a
+ON o.account_id = a.id
+WHERE a.name = 'Walmart'
+GROUP BY DATE_TRUNC('month', o.occurred_at)
+ORDER BY SUM(o.gloss_amt_usd) DESC;
