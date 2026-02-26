9ee35f76f3bec2e3d66abbc995bbbd8e41f01068
John Shea <sheajohnh@gmail.com>
2026-02-26 11:05:20 -0500

    Write 2nd query for case statement quiz

--- course_02_quiz_case_statement.sql
+++ course_02_quiz_case_statement.sql
@@ -2,3 +2,14 @@
 SELECT o.account_id, o.total_amt_usd, CASE WHEN o.total_amt_usd >= 3000 THEN 'Large' ELSE 'Small' END
 FROM orders AS o
 ORDER BY o.total_amt_usd;
+
+-- 2.Write a query to display the number of orders in each of three categories, based on the total number of items in each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
+SELECT
+  o.total,
+  CASE
+    WHEN o.total < 1000 THEN 'Less than 1000'
+    WHEN o.total >= 1000 AND o.total < 2000 THEN 'Between 1000 and 2000'
+    ELSE 'At Least 2000'
+  END
+FROM orders AS o
+ORDER BY o.total;
