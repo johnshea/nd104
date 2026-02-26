32094be2b379c3fcb26fb9d5864de68aa8448be0
John Shea <sheajohnh@gmail.com>
2026-02-26 13:13:30 -0500

    Write 6th query for case statement quiz

--- course_02_quiz_case_statement.sql
+++ course_02_quiz_case_statement.sql
@@ -59,3 +59,21 @@ JOIN sales_reps AS sr
 ON a.sales_rep_id = sr.id
 GROUP BY sr.ID, sr.name
 ORDER BY COUNT(o.id) DESC;
+
+-- 6. The previous didn't account for the middle, nor the dollar amount associated with the sales. Management decides they want to see these characteristics represented as well. We would like to identify top-performing sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales. The middle group has any rep with more than 150 orders or 500000 in sales. Create a table with the sales rep name, the total number of orders, total sales across all orders, and a column with top, middle, or low depending on these criteria. Place the top salespeople based on the dollar amount of sales first in your final table. You might see a few upset salespeople by this criteria!
+SELECT
+  sr.name AS sales_rep_name,
+  COUNT(o.id) AS order_count,
+  SUM(o.total_amt_usd) AS totals_sales_usd,
+  CASE
+    WHEN COUNT(o.id) > 200  OR SUM(o.total_amt_usd) > 750000 THEN 'top'
+    WHEN COUNT(o.id) > 150  OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
+    ELSE 'low'
+  END
+FROM orders AS o
+JOIN accounts AS a
+ON o.account_id = a.id
+JOIN sales_reps AS sr
+ON a.sales_rep_id = sr.id
+GROUP BY sr.ID, sr.name
+ORDER BY SUM(o.total_amt_usd) DESC;
