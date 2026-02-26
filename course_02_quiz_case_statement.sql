a7b9cc5cee03cf991a8a9d8b1523aabe6e999207
John Shea <sheajohnh@gmail.com>
2026-02-26 11:45:20 -0500

    Write 4th query for case statement quiz

--- course_02_quiz_case_statement.sql
+++ course_02_quiz_case_statement.sql
@@ -3,7 +3,7 @@ SELECT o.account_id, o.total_amt_usd, CASE WHEN o.total_amt_usd >= 3000 THEN 'La
 FROM orders AS o
 ORDER BY o.total_amt_usd;
 
--- 2.Write a query to display the number of orders in each of three categories, based on the total number of items in each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
+-- 2. Write a query to display the number of orders in each of three categories, based on the total number of items in each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
 SELECT
   o.total,
   CASE
@@ -29,3 +29,17 @@ JOIN accounts AS a
 ON o.account_id = a.id
 GROUP BY a.id, a.name
 ORDER BY SUM(o.total_amt_usd) DESC;
+
+-- 4. We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending customers listed first.
+SELECT
+  o.account_id,
+  SUM(o.total_amt_usd),
+  CASE
+    WHEN SUM(o.total_amt_usd) > 200000 THEN 'greater than 200,000'
+    WHEN SUM(o.total_amt_usd) >= 100000 AND SUM(o.total_amt_usd) <= 200000 THEN '200,000 and 100,000'
+    ELSE 'under 100,000'
+  END
+FROM orders AS o
+WHERE DATE_PART('year', o.occurred_at) BETWEEN 2016 AND 2017
+GROUP BY o.account_id
+ORDER BY SUM(o.total_amt_usd);
