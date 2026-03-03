1ac6a763865cc158e07edd213bc0cbc5f7694478
John Shea <sheajohnh@gmail.com>
2026-03-03 13:21:00 -0500

    Write 2nd query for quiz POSITION STRPOS

--- course_02_quiz_POSITION_STRPOS.sql
+++ course_02_quiz_POSITION_STRPOS.sql
@@ -4,3 +4,10 @@ SELECT
   SUBSTR(a.primary_poc, 1, STRPOS(a.primary_poc, ' ') - 1) AS first,
   SUBSTR(a.primary_poc, STRPOS(a.primary_poc, ' ') + 1) AS last
 FROM accounts AS a;
+
+-- 2. Now see if you can do the same thing for every rep name in the sales_reps table. Again provide first and last name columns.
+SELECT
+  sr.name,
+  SUBSTR(sr.name, 1, STRPOS(sr.name, ' ') - 1) AS first,
+  SUBSTR(sr.name, STRPOS(sr.name, ' ') + 1) AS last
+FROM sales_reps AS sr;
