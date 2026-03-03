b39d36045ee04984dc59a8f48a5a2c3a0dddd854
John Shea <sheajohnh@gmail.com>
2026-03-03 13:40:01 -0500

    Write 2ND query for quiz CONCAT STRPOS

--- course_02_quiz_CONCAT_STRPOS.sql
+++ course_02_quiz_CONCAT_STRPOS.sql
@@ -4,3 +4,10 @@ SELECT
   a.name,
   LOWER(LEFT(a.primary_poc, STRPOS(a.primary_poc, ' ') - 1)) || '.' || LOWER(SUBSTR(a.primary_poc, STRPOS(a.primary_poc, ' ') + 1)) || '@' || LOWER(a.name) || '.com' AS email
 FROM accounts AS a;
+
+-- 2. You may have noticed that in the previous solution some of the company names include spaces, which will certainly not work in an email address. See if you can create an email address that will work by removing all of the spaces in the account name, but otherwise, your solution should be just as in question 1. Some helpful documentation on String Functions and Operators(opens in a new tab).
+SELECT
+  a.primary_poc,
+  a.name,
+  LOWER(LEFT(a.primary_poc, STRPOS(a.primary_poc, ' ') - 1)) || '.' || LOWER(SUBSTR(a.primary_poc, STRPOS(a.primary_poc, ' ') + 1)) || '@' || REPLACE(LOWER(a.name), ' ', '') || '.com' AS email
+FROM accounts AS a;
