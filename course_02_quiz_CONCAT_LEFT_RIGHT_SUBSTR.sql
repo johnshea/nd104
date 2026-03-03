8fb4dcfe374f3167c1a158c172b66004001b8bab
John Shea <sheajohnh@gmail.com>
2026-03-03 12:30:30 -0500

    Write 2nd query for quiz CONCAT LEFT RIGHT SUBSTR

--- course_02_quiz_CONCAT_LEFT_RIGHT_SUBSTR.sql
+++ course_02_quiz_CONCAT_LEFT_RIGHT_SUBSTR.sql
@@ -3,3 +3,15 @@ SELECT CONCAT(sr.id, '_', r.name) AS EMP_ID_REGION
 FROM sales_reps AS sr
 JOIN region AS r
 ON sr.region_id = r.id;
+
+-- 2. From the accounts table, display the name of the client, the coordinate as concatenated (latitude, longitude), email id of the primary point of contact as <first letter of the primary_poc><last letter of the primary_poc>@<extracted name and domain from the website>
+SELECT
+  a.name,
+  CONCAT('(', a.lat, ', ', a.long, ')') AS coordinate,
+  CONCAT(
+    LOWER(LEFT(a.primary_poc, 1)),
+    LOWER(RIGHT(a.primary_poc, 1)),
+    '@',
+    SUBSTR(a.website, 5)
+  ) AS email_id
+FROM accounts AS a;
